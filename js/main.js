804b22d5a5ce1243bb1018960f970ebe6c55a2c4
Micah <micah.wood@gmail.com>
2013-11-13 21:15:25 -0700

    tweaked layout and hid the resume at the start to prevent it overflowing on to the body

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 
   var $resume = $('#resume'),
       $navbar = $('.navbar'),
+      $images = $('#images'),
       resumeDisplayed = false;
 
   $('.nav').on('click', 'li', function(event) {
@@ -18,12 +19,18 @@ $(document).ready(function() {
     }
   });
 
+  if (window.innerWidth <= 765) {
+    $('.nav').removeClass('wider');
+  }
+
   var showResume = function showResume() {
     if (!resumeDisplayed) {
       var height = window.innerHeight * 0.8;
+      $resume.show();
       $resume.animate({top: '0px'}, 800);
       $navbar.animate({top: height}, 800);
       resumeDisplayed = !resumeDisplayed;
+      setTimeout(function() { $images.hide();}, 800);
     }
   };
 
@@ -32,6 +39,8 @@ $(document).ready(function() {
       $resume.animate({top: '-100%'}, 600);
       $navbar.animate({top: '0px'}, 600);
       resumeDisplayed = !resumeDisplayed;
+      setTimeout(function() { $resume.hide();}, 800);
+      $images.show();
     }
   };
 
