5f701cca623f4bdfc419401a7178b0c2be8124c8
Micah <micah.wood@gmail.com>
2013-12-01 21:24:57 -0700

    looks like i changed the grunt file at some point

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,6 @@ module.exports = function(grunt) {
 	require('load-grunt-tasks')(grunt, 'grunt-*');
 
 	grunt.initConfig({
-    //pkg: grunt.file.readJSON('package.json'),
 		uglify: {
 			options: {},
 			core: {
