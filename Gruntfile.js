9536807810e1aef1afca1a69e6a1dec41b5b9857
Micah <micah.wood@softwaretechnologygroup.com>
2014-05-09 15:12:26 -0600

    fixed grunt file identation

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,27 +1,27 @@
 module.exports = function(grunt) {
-	require('load-grunt-tasks')(grunt, 'grunt-*');
+  require('load-grunt-tasks')(grunt, 'grunt-*');
 
-	grunt.initConfig({
-		uglify: {
-			options: {},
-			core: {
-				src: [
-          'src/js/bootstrap.js',
-          'src/js/venobox.min.js',
-          'src/js/main.js'
+  grunt.initConfig({
+    uglify: {
+      options: {},
+      core: {
+        src: [
+        'src/js/bootstrap.js',
+        'src/js/venobox.min.js',
+        'src/js/main.js'
         ],
         dest: 'js/core.js'
-			}
-		},
+      }
+    },
     less: {
       options: {
         yuicompress: true
       },
       core: {
         src: [
-          'src/css/bootstrap.css',
-          'src/css/venobox.css',
-          'src/css/main.css'
+        'src/css/bootstrap.css',
+        'src/css/venobox.css',
+        'src/css/main.css'
         ],
         dest: 'css/core.css'
       }
@@ -53,7 +53,7 @@ module.exports = function(grunt) {
         }
       },
     }
-	});
+  });
 
   grunt.registerTask('dev', function () {
     grunt.task.run([
