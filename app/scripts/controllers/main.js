c369e631a7ed464f8bf937517b00a0b7084838e5
Micah <micah.wood@softwaretechnologygroup.com>
2014-05-11 12:48:38 -0600

    cleaned up old image dirs

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,11 +3,13 @@
 angular.module('jenasiteApp')
   .controller('GalleryCtrl', function ($scope) {
     $scope.images = [
-      {mainUrl: "images/DSC_1059.jpg", thumbnail: "images/original/thumbs/DSC_1059.jpg", title:"A picture"},
-      {mainUrl: "images/DSC_1077.jpg", thumbnail: "images/original/thumbs/DSC_1077.jpg", title:"another picture"},
-      {mainUrl: "images/DSC_1087.jpg", thumbnail: "images/original/thumbs/DSC_1087.jpg", title:"pretty picture"},
-      {mainUrl: "images/DSC_1089.jpg", thumbnail: "images/original/thumbs/DSC_1089.jpg", title:"an ok picture"},
+      {mainUrl: "images/DSC_1059.jpg", thumbnail: "images/thumbs/DSC_1059.jpg", title:"A picture"},
+      {mainUrl: "images/DSC_1077.jpg", thumbnail: "images/thumbs/DSC_1077.jpg", title:"another picture"},
+      {mainUrl: "images/DSC_1087.jpg", thumbnail: "images/thumbs/DSC_1087.jpg", title:"pretty picture"},
+      {mainUrl: "images/DSC_1089.jpg", thumbnail: "images/thumbs/DSC_1089.jpg", title:"an ok picture"},
     ];
+
+    // $scope.setTransparency = function
   })
   .directive('jenaVenobox', function(){
     
