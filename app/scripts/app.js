63642053c3220fbe17e60ec33c39d98b0f173f60
Micah <micah.wood@softwaretechnologygroup.com>
2014-06-25 11:42:19 -0600

    added html5 location mode

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -6,7 +6,8 @@ angular
     'ngRoute',
     'ngAnimate'
   ])
-  .config(['$routeProvider', function ($routeProvider) {
+  .config(['$routeProvider', 'locationProvider', function ($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
